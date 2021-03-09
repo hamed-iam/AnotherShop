ef9048375b6ddbc1f6789bf0d3e08cf99ca0758d
Hamed Ghazali <hamedg20@yahoo.com>
2021-03-09 12:31:10 +0330

    everythingbutpayment

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -8,10 +8,32 @@ import {
 import { loadStripe } from '@stripe/stripe-js';
 import Review from './Review';
 
+const stripePromise = loadStripe('...');
+
 const PaymentForm = ({ checkoutToken }) => {
   return (
     <>
       <Review checkoutToken={checkoutToken} />
+      <Divider />
+      <Typography variant="h6" gutterBottom style={{ margin: '20px 0' }}>
+        Payment Method
+      </Typography>
+
+      <Elements stripe={stripePromise}>
+        <ElementConsumer>
+          {(elements, stripe) => (
+            <form>
+              <CardElement />
+              <br />
+              <br />
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                <Button></Button>
+                <Button></Button>
+              </div>
+            </form>
+          )}
+        </ElementConsumer>
+      </Elements>
     </>
   );
 };
